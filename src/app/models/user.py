6e369e5772138bf2c13954e19be32262c649dbaa
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-09-27 14:38:51 +0100

    Create user.py

---
+++ src/app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship  # ← à importer
+from ..database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    accounts = relationship("Account", back_populates="owner")  # ← ajoute cette ligne
+
