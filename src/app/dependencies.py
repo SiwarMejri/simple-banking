1ae53ba852322ede9c1077a04550d7de44d6759a
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-13 15:42:30 +0100

    Update dependencies.py

--- src/app/dependencies.py
+++ src/app/dependencies.py
@@ -1,12 +1,8 @@
 # src/app/dependencies.py
 import os
-from fastapi import Depends
 from .main import get_current_user as real_get_current_user
 
 TESTING = os.environ.get("TESTING", "0") == "1"
-
-def get_test_user():
-    return {"preferred_username": "testuser", "realm_access": {"roles": ["admin"]}}
 
 def get_user_dep():
     """
@@ -14,5 +10,5 @@ def get_user_dep():
     En mode TESTING, renvoie un user factice.
     """
     if TESTING:
-        return get_test_user()
+        return {"preferred_username": "testuser", "realm_access": {"roles": ["admin"]}}
     return real_get_current_user()
