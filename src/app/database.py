31a524abf544328e6ce3ad7071f3f2b4fb504ba0
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-17 11:02:06 +0100

    Update database.py

--- src/app/database.py
+++ src/app/database.py
@@ -1,11 +1,11 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:admin@localhost/banking"
-)
+load_dotenv()  # Charge les variables depuis .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
