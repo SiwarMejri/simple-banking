b1616813a092b3d12591c7281fd9999a2d5cf222
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-19 19:17:53 +0100

    Update account.py

--- src/app/models/account.py
+++ src/app/models/account.py
@@ -14,17 +14,3 @@ class Account(Base):
     # Relation vers User
     owner = relationship("User", back_populates="accounts")
     
-# Relation avec Transaction
-    outgoing_transactions = relationship(
-        "src.app.models.transaction.Transaction",
-        foreign_keys="[src.app.models.transaction.Transaction.origin_account]",
-        back_populates="origin",
-        cascade="all, delete"
-    )
-
-    incoming_transactions = relationship(
-        "src.app.models.transaction.Transaction",
-        foreign_keys="[src.app.models.transaction.Transaction.destination_account]",
-        back_populates="destination",
-        cascade="all, delete"
-    )
