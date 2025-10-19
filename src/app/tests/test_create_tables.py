e47e8974914dc5362fa6be65d88eebfedab09590
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-19 16:39:15 +0100

    Update test_create_tables.py

--- src/app/tests/test_create_tables.py
+++ src/app/tests/test_create_tables.py
@@ -1,17 +1,15 @@
 # tests/test_create_tables.py
-from sqlalchemy import inspect
 from src.app import create_tables
-from src.app.database import Base, engine
+from src.app.database import engine, Base
+import pytest
 
 def test_tables_created():
-    # 🔹 Supprimer les tables existantes
+    # Supprimer les tables si existantes
     Base.metadata.drop_all(bind=engine)
-    # 🔹 Recréer les tables
+    # Recréer
     create_tables.create_all_tables()
-    # 🔹 Vérifier la présence des tables
-    inspector = inspect(engine)
+    # Vérifier que les tables existent
+    inspector = create_tables.inspect(engine)
     tables = inspector.get_table_names()
-
     assert "users" in tables
     assert "accounts" in tables
-    assert "transactions" in tables
