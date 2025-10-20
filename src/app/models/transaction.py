0a2d7156a015c02796edeb85c72dfb00a9d2468c
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-20 12:32:35 +0100

    Update transaction.py

--- src/app/models/transaction.py
+++ src/app/models/transaction.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from src.app.database import Base
 
+
 class Transaction(Base):
     __tablename__ = "transactions"
     __table_args__ = {'extend_existing': True}
@@ -14,4 +15,10 @@ class Transaction(Base):
     timestamp = Column(DateTime, default=datetime.utcnow)
     origin_account = Column(String, ForeignKey("accounts.id"), nullable=True)
     destination_account = Column(String, ForeignKey("accounts.id"), nullable=True)
- 
+
+    # Relations vers Account
+    origin_acc = relationship("Account", foreign_keys=[origin_account], back_populates="outgoing_transactions")
+    dest_acc = relationship("Account", foreign_keys=[destination_account], back_populates="incoming_transactions")
+
+    def __repr__(self):
+        return f"<Transaction(type='{self.type}', amount={self.amount})>"
