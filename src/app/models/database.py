c952f6ab4da5453fc00794cb8e1829c0678dab93
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-20 19:50:58 +0100

    Update database.py

--- src/app/models/database.py
+++ src/app/models/database.py
@@ -1,9 +1,11 @@
 # src/app/models/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from src.app.models.base import Base  # ✅ utilisation de la même Base
+from src.app.models.base import Base  # Import de la Base unique
 
 DATABASE_URL = "sqlite:///./banking.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
