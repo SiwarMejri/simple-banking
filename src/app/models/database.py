e99b7cb1505bc3fceee488c927c5977871432847
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-20 19:48:47 +0100

    Update database.py

--- src/app/models/database.py
+++ src/app/models/database.py
@@ -1,10 +1,9 @@
 # src/app/models/database.py
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from src.app.models.base import Base  # ✅ utilisation de la même Base
 
-DATABASE_URL = "sqlite:///./banking.db"  # ou PostgreSQL selon ton ENV
+DATABASE_URL = "sqlite:///./banking.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()  # ⚠️ Base unique pour tous les modèles
