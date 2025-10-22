1f9e25af60a3501178f7e9f3d89da7af36eadc8f
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-22 18:07:00 +0100

    Update transaction.py

--- src/app/models/transaction.py
+++ src/app/models/transaction.py
@@ -15,7 +15,7 @@ class Transaction(Base):
     origin_account = Column(String, ForeignKey("accounts.id"), nullable=True)
     destination_account = Column(String, ForeignKey("accounts.id"), nullable=True)
 
-    # Relations vers Account
+    # Relations avec chaînes
     origin_acc = relationship("Account", foreign_keys=[origin_account], back_populates="outgoing_transactions")
     dest_acc = relationship("Account", foreign_keys=[destination_account], back_populates="incoming_transactions")
 
