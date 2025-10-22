21cc6f48211b1da80b8c59e2dea118fb37b6d2d2
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-23 00:20:22 +0100

    Update __init__.py

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,5 +1,7 @@
+# Fichier CORRECT
 from .user import UserModel
 from .account import AccountModel
 from .transaction import TransactionModel
 
+# Exporter les noms corrects
 __all__ = ["UserModel", "AccountModel", "TransactionModel"]
