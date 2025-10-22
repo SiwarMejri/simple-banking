4e40515e1bf51cbd3fcb1f8b96fd41809ccaf9ba
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-22 19:11:46 +0100

    Update create_tables.py

--- src/app/create_tables.py
+++ src/app/create_tables.py
@@ -1,18 +1,9 @@
-# src/app/create_tables.py
-from sqlalchemy import inspect
-from src.app.database import Base, engine
-from src.app.models.user import User
-from src.app.models.account import Account
-from src.app.models.transaction import Transaction
-
+from src.app.models.database import engine
+from src.app.models.base import Base
+# Importer les modèles pour s'assurer qu'ils sont enregistrés
+from src.app.models import user, account, transaction
 
 def create_all_tables():
     """Crée toutes les tables dans la base de données."""
     Base.metadata.create_all(bind=engine)
     print("✅ Tables créées avec succès dans la base de données.")
-
-
-if __name__ == "__main__":
-    create_all_tables()
-    inspector = inspect(engine)
-    print("📋 Tables présentes :", inspector.get_table_names())
