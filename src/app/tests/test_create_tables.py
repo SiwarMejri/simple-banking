b4f9fb92a65b5e1d7d8baae1a1328b8b2331270d
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-22 19:43:51 +0100

    Update test_create_tables.py

--- src/app/tests/test_create_tables.py
+++ src/app/tests/test_create_tables.py
@@ -1,6 +1,6 @@
-from src.app import create_tables
-from src.app.database import Base, engine
-import pytest
+from app.models.base import Base
+from app.models.database import engine
+from app import create_tables
 
 def test_tables_created():
     # Supprimer les tables si existantes
@@ -8,7 +8,7 @@ def test_tables_created():
     # Recréer
     create_tables.create_all_tables()
     # Vérifier que les tables existent
-    inspector = create_tables.inspect(engine)
+    inspector = create_tables.get_inspector(engine)  # Utilisation de la fonction renommée
     tables = inspector.get_table_names()
     assert "users" in tables
     assert "accounts" in tables
