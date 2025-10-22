b9ff1f1f0fb52fa4b25010a50471ce81502c2b3b
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-23 00:49:31 +0100

    Update crud.py

--- src/app/crud.py
+++ src/app/crud.py
@@ -1,41 +1,41 @@
 from sqlalchemy.orm import Session
-from src.app.models import UserModel, AccountModel, TransactionModel # Correction des imports : suppression de src.app
+from src.app.models import UserModel, AccountModel, TransactionModel
 from schemas import UserCreate, AccountCreate
 
 def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+    return db.query(UserModel).filter(UserModel.id == user_id).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(User).offset(skip).limit(limit).all()
+    return db.query(UserModel).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: UserCreate):
-    db_user = User(name=user.name, email=user.email, password=user.password)
+    db_user = UserModel(name=user.name, email=user.email, password=user.password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
 def get_account(db: Session, account_id: str):
-    return db.query(Account).filter(Account.id == account_id).first()
+    return db.query(AccountModel).filter(AccountModel.id == account_id).first()
 
 def get_accounts(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Account).offset(skip).limit(limit).all()
+    return db.query(AccountModel).offset(skip).limit(limit).all()
 
 def create_account(db: Session, account: AccountCreate, user_id: int):
-    db_account = Account(id=account.id, balance=account.balance, owner_id=user_id)
+    db_account = AccountModel(id=account.id, balance=account.balance, owner_id=user_id)
     db.add(db_account)
     db.commit()
     db.refresh(db_account)
     return db_account
 
 def get_transaction(db: Session, transaction_id: int):
-    return db.query(Transaction).filter(Transaction.id == transaction_id).first()
+    return db.query(TransactionModel).filter(TransactionModel.id == transaction_id).first()
 
 def get_transactions(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Transaction).offset(skip).limit(limit).all()
+    return db.query(TransactionModel).offset(skip).limit(limit).all()
 
 def create_transaction(db: Session, transaction: dict):
-    db_transaction = Transaction(
+    db_transaction = TransactionModel(
         type=transaction.get("type"),
         amount=transaction.get("amount"),
         origin=transaction.get("origin"),
