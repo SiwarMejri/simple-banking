a3df81f74bd04399b1db07972ada0980278f6e1b
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-22 23:00:04 +0100

    Update account.py

--- src/app/models/account.py
+++ src/app/models/account.py
@@ -1,12 +1,14 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from .base import Base
+from src.app.database import Base
 
-class AccountModel(Base):
+class Account(Base):
     __tablename__ = "accounts"
 
     id = Column(String, primary_key=True, index=True)
     balance = Column(Float, default=0.0)
-    owner_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
-    user = relationship("UserModel", back_populates="accounts")
+    # Relations
+    owner = relationship("User", back_populates="accounts")
+    transactions = relationship("Transaction", back_populates="account")
