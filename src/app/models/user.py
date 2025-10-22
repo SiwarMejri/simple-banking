463bab48011592a1a35f1d8866f2dae48e366865
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-22 23:20:51 +0100

    Update user.py

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -2,12 +2,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from src.app.database import Base
 
-class User(Base):
+class UserModel(Base):  # Garder UserModel comme nom de classe
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
-    # Relations - Correction des imports
+    # Relations
     accounts = relationship("Account", back_populates="owner")
