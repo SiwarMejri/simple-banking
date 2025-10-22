912c836893525b2e2fbb72ac18f3e0855e53d255
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-22 23:21:13 +0100

    Update __init__.py

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,4 +1,11 @@
 from .base import Base
-from .user import UserModel as User
-from .account import AccountModel as Account
-from .transaction import TransactionModel as Transaction
+from .user import UserModel
+from .account import AccountModel
+from .transaction import TransactionModel
+
+# Exporter avec des noms simplifiés si nécessaire
+User = UserModel
+Account = AccountModel
+Transaction = TransactionModel
+
+__all__ = ["Base", "User", "Account", "Transaction", "UserModel", "AccountModel", "TransactionModel"]
