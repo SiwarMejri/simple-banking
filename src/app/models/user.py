d4938475ac4b5cc95d4db782cb896d25440fc550
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-23 00:00:16 +0100

    Update user.py

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -2,8 +2,9 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from src.app.database import Base
 
-class UserModel(Base):  # Doit s'appeler UserModel
+class UserModel(Base):
     __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}  # Ajoutez cette ligne
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
