518ebebe353742c39279f47bc4778ab171405bd4
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-22 17:54:01 +0100

    Update database.py

--- src/app/models/database.py
+++ src/app/models/database.py
@@ -3,18 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.app.models.base import Base
 import os
+import tempfile
 
 # --- Choix du mode : test ou normal ---
 TESTING = os.getenv("TESTING", "0") == "1"
 
 if TESTING:
-    # Base en mémoire (volatile) pour exécution de tests
+    # ⚡ Base en mémoire (DB volatile et propre à chaque session de test)
     DATABASE_URL = "sqlite:///:memory:"
 else:
-    # Base locale par défaut
     DATABASE_URL = "sqlite:///./banking.db"
 
 # --- Création de l'engine ---
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
