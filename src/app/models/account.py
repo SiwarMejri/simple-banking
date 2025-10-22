b97ee2ca634052d694773cf479a0d0d47691456c
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-22 19:09:35 +0100

    Update account.py

--- src/app/models/account.py
+++ src/app/models/account.py
@@ -1,28 +1,12 @@
-# src/app/models/account.py
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from src.app.models.database import Base
+from .base import Base
 
-class Account(Base):
+class AccountModel(Base):
     __tablename__ = "accounts"
-    __table_args__ = {'extend_existing': True}
 
     id = Column(String, primary_key=True, index=True)
     balance = Column(Float, default=0.0)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=True)
 
-    # Relations avec chaînes pour éviter les conflits d'imports
-    owner = relationship("User", back_populates="accounts")
-    outgoing_transactions = relationship(
-        "Transaction",
-        foreign_keys="[Transaction.origin_account]",
-        back_populates="origin_acc"
-    )
-    incoming_transactions = relationship(
-        "Transaction",
-        foreign_keys="[Transaction.destination_account]",
-        back_populates="dest_acc"
-    )
-
-    def __repr__(self):
-        return f"<Account(id='{self.id}', balance={self.balance})>"
+    user = relationship("UserModel", back_populates="accounts")
