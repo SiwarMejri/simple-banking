a2b5852898ede3efcf5ed3a90b471f14adbc9d48
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-23 00:07:06 +0100

    Update __init__.py

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,11 +1,5 @@
-from .base import Base
 from .user import UserModel
 from .account import AccountModel
 from .transaction import TransactionModel
 
-# Créer des alias pour une utilisation simplifiée
-User = UserModel
-Account = AccountModel
-Transaction = TransactionModel
-
-__all__ = ["Base", "User", "Account", "Transaction", "UserModel", "AccountModel", "TransactionModel"]
+__all__ = ["UserModel", "AccountModel", "TransactionModel"]
