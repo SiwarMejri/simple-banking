f3060a933dcf701844d317428930bb791fdf78c0
SiwarMejri <151733065+SiwarMejri@users.noreply.github.com>
2025-10-23 16:50:27 +0100

    Update models.py

--- src/app/models/models.py
+++ src/app/models/models.py
@@ -1,3 +1,4 @@
-from src.app.models.user import User
-from src.app.models.account import Account
-from src.app.models.transaction import Transaction
+# src/app/models/models.py
+from src.app.models.user import UserModel as User
+from src.app.models.account import AccountModel as Account
+from src.app.models.transaction import TransactionModel as Transaction
